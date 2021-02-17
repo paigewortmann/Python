a31193dd9de7d84cf942ee38056b4eab696a2e86
paigewortmann <78603285+paigewortmann@users.noreply.github.com>
2021-02-17 02:08:37 -0500

    Update python2.icon.py

--- python2.icon.py
+++ python2.icon.py
@@ -1,60 +1,99 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Feb  6 17:42:03 2021
-
-@author: 17244
-"""
-
-"""Declare global variable for access
-by all functions in the script """
+'''
+ Paige Wortmann
+ Python 2 - DAT-129
+ Icon Manipulation
+ a program that takes in 1s and 0s and displays a simple character icon
+'''
 
 
-def number_validation():
-    '''collects user numbers and prints them vertically'''
-    lst = []
+import sys
+
+lst = []
+
+def number_validation(row):
+    '''collects 10 numbers from user which goes into its own list'''
     
-    n = 10
-    # asks for a new row 10 times
+   # for i in range(10):
+       
+    counter = 1
+       
+    while counter < row + 1:
+        raw_input = input("Please enter 10 digits, each followed by a space: ")
+       
+      #  raw_input = input("Please enter 10 digits: ")
+
+        if len(raw_input) != 20:
+            if len(raw_input) < 20:
+                    raw_input = (input("I dont have enough values, lets try again: "))
+            if len(raw_input) > 20:
+                    raw_input = (input("I have too many values, lets try again: "))
+        else:
+            # adds each row into total icon list, each number has its own index
+            lst.append(raw_input.split())
+            counter += 1
+                    
     
-    for i in range(0 , n):
-        user_row = input("Please enter row of 10 digits: ")
-        
-        new_row = lst.append([user_row])
-        
+    
+def encoding(number):
+    ''' turns number into either a blank or "*" '''
+    if number == '0':
+        return " "
+    if number == '1':
+        return "*"
+    
+    # if number is not a 1 or 0, will fill in as a space instead
+    else:
+        return " "
+    
+def double_encoding(number):
+    ''' doubles how many characters are assigned to one number'''
+    if number == '0':
+        return '  '
+    if number == '1':
+        return '**'
+    else:
+        return ' '
             
-    
-    print() #space
-    return new_row
-
-def encoding():
-    
-# work in progress??
-    encoding_lst = number_validation()
-    for i in encoding_lst:
-        if i == '1':
-            encoding_lst = '*'
-        elif i == '0':
-            encoding_lst = ' '
-    
-# gets rid of "[]" and "," from lst
-    for i in encoding_lst:
-        print(' '.join(map(str, i)))
-        return encoding_lst
         
-def printing():
     
-        
-      
-
-            
-
+def printing(icon):
+    '''takes number-validated row and encodes into simple characters'''
+    for row in icon:
+        for number in row:
+            print(encoding(number), end='') # encodes one row at a time
+        print('\r') # carriage return
+    
+def double_printing(icon):
+    for row in icon:
+        for number in row:
+            print(double_encoding(number), end='') 
+        print('\r')
+    
+  #  one_row(10)
+    
 def main():
-    user_icon = number_validation()
-    print(user_icon)
+    print('Hello and welcome to Icon Translation!')
+    print('This program will take 10 "1"s and "0"s and display a pretty')
+    print('picture using simple character-based output.')
+    print()
+    print('disclaimer: any character that is not a 1 or 0 will be interpreted')
+    print('as a blank space, and ill write yor name')
+   
+    number_validation(10) # asks user and validates input 10 times
+    printing(lst)
     
-   # encoding(user_icon)
+    double = input('We can double the size if you would like (yes/no): ')
+    if double == 'yes':
+        double_printing(lst)
+        print('Hope you like it! Thank you.')
+        sys.exit(0)
+        
+    if double == 'no':
+        print('Thanks, come again soon.')
+        sys.exit(0)
     
-
+    
   
     
 if __name__ == '__main__':
